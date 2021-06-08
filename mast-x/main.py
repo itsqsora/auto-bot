2e98377604d45df1b4bd6f0c1ffcf622c61ecded
Soranan Rungtanee <62444295+soranankiw1@users.noreply.github.com>
2021-06-08 17:42:16 +0700

    Add files via upload

--- mast-x/main.py
+++ mast-x/main.py
@@ -5,9 +5,15 @@ from webdriver_manager import driver
 from webdriver_manager import chrome
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import ActionChains
 
-user = input("Enter your username: ")
-pw = input("Enter your password: ")
+
+# user = input("Enter your username: ")
+# pw = input("Enter your password: ")
+
+user = input('Enter your username: ')
+pw = 'Enter your password'
+print('Keep In Mind This is could not save your password, Until your edit at 14th and 15th line into Your password and username.')
 
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -24,3 +30,5 @@ sleep(1)
 print("Okay...")
 sleep(1)
 driver.get('https://xhamsterlive.com/PamelaWet')
+button_box = driver.find_elements_by_xpath('//*[@id="body"]/div[2]/div/div/div[2]/div[1]/button')
+button_box[0].click()
